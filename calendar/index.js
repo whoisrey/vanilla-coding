940140c4919cec0ce3e475b925392fb18c4404e3
holaleche <whoisleche@gmail.com>
2023-12-12 21:13:35 +0900

    Feat: 현재 요일, 날짜, 월, 연도 표기

--- calendar/index.js
+++ calendar/index.js
@@ -1,1 +1,31 @@
-console.log("hello, vanilla.");
+const month = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+const day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+const today = new Date();
+const date = today.getDate();
+
+const currentYear = today.getFullYear();
+const currentMonth = month[today.getMonth()];
+const currentDay = day[today.getDay()];
+const currentDate = today.getDate();
+
+const dayAndDate = document.querySelector(".day-date");
+const clickedDay = document.getElementById("day");
+const clickedDate = document.getElementById("date");
+const monthAndYear = document.getElementById("month-year");
+
+clickedDay.innerText = `${currentDay}`;
+clickedDate.innerText = `${currentDate}`;
+monthAndYear.innerText = `${currentMonth} ${currentYear}`;
