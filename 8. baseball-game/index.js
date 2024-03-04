7a97b9880f72ead5def6d7930bf8db05f3036e99
holaleche <whoisleche@gmail.com>
2024-03-04 11:27:54 +0900

    feat: 각 자리수 숫자 비교

--- 8. baseball-game/index.js
+++ 8. baseball-game/index.js
@@ -1,6 +1,10 @@
 const startBtn = document.querySelector(".start-btn");
 const container = document.querySelector(".container");
 const randomNum = document.querySelector(".random-number");
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+
 function makeRandomNum() {
   const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let a = 0;
@@ -19,10 +23,30 @@ function startGame() {
   makeRandomNum();
 }
 
+function compareNum(first, second) {
+  const firstArr = String(first).split("");
+  const secondArr = String(second).split("");
+  let strikes = 0;
+  let balls = 0;
+  for (let i = 0; i < firstArr.length; i++) {
+    for (let j = 0; j < secondArr.length; j++) {
+      if (i === j) {
+        if (firstArr[i] === second[j]) {
+          strikes++;
+        }
+      } else {
+        if (firstArr[i] === second[j]) {
+          balls++;
+        }
+      }
+    }
+  }
+  console.log(strikes);
+  console.log(balls);
+  strikes = 0;
+  balls = 0;
+}
 startBtn.addEventListener("click", startGame);
-
-const form = document.querySelector("form");
-const input = document.querySelector("input");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -31,8 +55,8 @@ form.addEventListener("submit", function (e) {
   if (100 < userNum && userNum < 1000) {
     console.log(userNum);
     console.log(computerNum);
+    compareNum(userNum, computerNum);
   } else {
     alert("세 자리 숫자를 입력하시오.");
-    userNum = "";
   }
 });
