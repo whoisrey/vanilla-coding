99dca6bf831ee2163192bc26a361b3ade4b23379
holaleche <whoisleche@gmail.com>
2024-03-04 10:40:57 +0900

    feat: 시작 버튼 기능 추가

--- 8. baseball-game/index.js
+++ 8. baseball-game/index.js
@@ -1,1 +1,15 @@
-console.log("hello, vanilla.");
+const startBtn = document.querySelector(".start-btn");
+const container = document.querySelector(".container");
+
+startBtn.addEventListener("click", function () {
+  container.style.display = "block";
+  const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let a = 0;
+  for (let i = 1; i < num.length; i++) {
+    a = num[Math.floor(Math.random() * 8)];
+  }
+  let b = Math.floor(Math.random() * 9);
+  let c = Math.floor(Math.random() * 9);
+  const randomNum = `${a}${b}${c}`;
+  console.log(randomNum);
+});
